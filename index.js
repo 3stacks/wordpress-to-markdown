866dad56659960cefcd65d8de4f0ad6125942d4f
Luke Boyle <luke.boyle.au@gmail.com>
2017-05-07 19:04:00 +1000

    chore: start converting to JSON

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const dump = fs.readFileSync('./forge.sql').toString();
+const dump = fs.readFileSync('./dump.json').toString();
 
 const REGEX = {
     DUMP_SECTION_HEADINGS: /(--\n-- Dumping data for table `.*`\n--)/igm,
@@ -7,27 +7,16 @@ const REGEX = {
     STRUCTURE_SECTION_HEADINGS: /(--\n-- Table structure for table `.*`\n--)/igm
 };
 
-/**
- * @param {String} string
- * @param {String|RegExp} substring
- */
-function getSections(string, substring) {
-    const sections = string.split(substring);
-    return sections;
-}
+const posts = JSON.parse(dump.slice(dump.indexOf('[')));
 
-const sections = getSections(dump, REGEX.DUMP_SECTION_HEADINGS);
+const out = [];
 
-const dumpData = sections.map(section => {
-    return section.split(REGEX.STRUCTURE_SECTION_HEADINGS)[0];
-});
-
-const postSection = dumpData.filter(section => {
-    const regexTestResult = REGEX.POSTS_SECTION_DATA.test(section);
-    if (regexTestResult) {
-        console.log(section);
+const parsedPosts = posts.map((post, index) => {
+    if (index === 25) {
+        out.push(JSON.stringify(post));
     }
-    return regexTestResult;
+    return post
 });
 
-fs.writeFileSync('dump.md', postSection);
+fs.writeFileSync('dump.md', parsedPosts);
+fs.writeFileSync('out.md', out);
