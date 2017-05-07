6f07a7ded55ca0f82725890c230f4c3c61a1547e
Luke Boyle <luke.boyle.au@gmail.com>
2017-05-08 00:39:22 +1000

    feat: add initial parsing and markdown generating

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const dump = fs.readFileSync('./dump.json').toString();
+const toMarkdown = require('to-markdown');
+const getMonth = require('date-fns/get_month');
+const getYear = require('date-fns/get_year');
+const shelljs = require('shelljs');
 
 const REGEX = {
     DUMP_SECTION_HEADINGS: /(--\n-- Dumping data for table `.*`\n--)/igm,
@@ -9,14 +13,57 @@ const REGEX = {
 
 const posts = JSON.parse(dump.slice(dump.indexOf('[')));
 
-const out = [];
-
-const parsedPosts = posts.map((post, index) => {
-    if (index === 25) {
-        out.push(JSON.stringify(post));
-    }
-    return post
+const parsedPosts = posts.map(post => {
+    return Object.assign({}, post, {
+        template: `
+            # ${post.post_title}
+            
+            Posted on ${post.post_date}
+            
+            ${toMarkdown(post.post_content)}
+        `
+    })
 });
 
-fs.writeFileSync('dump.md', parsedPosts);
-fs.writeFileSync('out.md', out);
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const datedPosts = parsedPosts.reduce((acc, curr) => {
+    const datePosted = new Date(curr.post_date);
+    const yearPosted = getYear(datePosted);
+    const monthPosted = months[datePosted.getMonth()];
+    return Object.assign({}, acc, {
+       [yearPosted]: {
+           [monthPosted]: {
+               posts: [
+                   ...acc[yearPosted][monthPosted][posts],
+                   curr
+               ]
+           }
+       }
+    });
+}, {});
+
+function prepareDirectories() {
+    shelljs.exec('mkdir', '-p', `myProject/{src,doc,tools,db}`);
+}
+
+parsedPosts.forEach(post => {
+    const datePosted = new Date(post.post_date);
+    const yearPosted = getYear(datePosted);
+    const monthPosted = months[datePosted.getMonth()];
+    shelljs.exec(`mkdir ./${yearPosted}/${monthPosted}`);
+    fs.writeFileSync(`${yearPosted}/${monthPosted}/${post.post_title}.md`, post.template);
+});
