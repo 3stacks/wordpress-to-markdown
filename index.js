f31cfb310d2633e74d85a1246ed30a228a3fca21
Luke Boyle <luke.boyle.au@gmail.com>
2017-12-24 09:25:06 +1100

    feat: add type filtering and meta table

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const allege = require('allege');
 const chalk = require('chalk');
 const toMarkdown = require('to-markdown');
 const getYear = require('date-fns/get_year');
@@ -10,7 +11,7 @@ const outputDir = !!rawOutputDir ? rawOutputDir.slice(rawOutputDir.indexOf('=') 
 
 const theFile = process.argv.find(arg => arg.includes('.json'));
 if (!theFile) {
-    console.error(chalk.bgRed.white('Could not detect a json file name in command. Use the command like this: wordpress-to-markdown [input] (e.g. fileName.json)'));
+    console.error(chalk.bgRed.black('Could not detect a json file name in command. Use the command like this: wordpress-to-markdown [input] (e.g. fileName.json)'));
     return;
 }
 const dumpPath = `${process.cwd()}/${theFile}`;
@@ -19,19 +20,33 @@ let dumpContents;
 try {
     dumpContents = fs.readFileSync(dumpPath).toString();
 } catch(e) {
-    console.error(chalk.bgRed.white(`Could not find file '${theFile}' in ${dumpPath}, please check the fileName/path and try again.`));
+    console.error(chalk.bgRed.black(`Could not find file '${theFile}' in ${dumpPath}, please check the fileName/path and try again.`));
     return;
 }
 
 const posts = JSON.parse(dumpContents.slice(dumpContents.indexOf('[')));
+const typesToFilter = process.argv.find(arg => arg.includes('--filter-type'));
 
 const parsedPosts = posts.reduce((acc, curr) => {
+    if (!!typesToFilter) {
+        const types = typesToFilter.replace('--filter-type=', '').split(',');
+
+        if (allege(curr.post_type).isAnyOf(...types)) {
+            return acc;
+        }
+    }
     if (curr.post_content.length > 0) {
         const postContent = Object.assign({}, curr, {
             template: `
 # ${curr.post_title}\n
 
-## Posted on ${curr.post_date}\n
+| Metadata name | Value |
+| --------- | ------ |
+| post_title | ${curr.post_title} | 
+| post_date | ${curr.post_date} | 
+| post_modified | ${curr.post_modified} | 
+| post_status | ${curr.post_status} | 
+| post_type | ${curr.post_type} |
 
 ${toMarkdown(curr.post_content)}
         `
@@ -51,7 +66,7 @@ ${toMarkdown(curr.post_content)}
 parsedPosts.forEach(post => {
     const datePosted = new Date(post.post_date);
     const yearPosted = getYear(datePosted);
-    const monthPosted = datePosted.getMonth() + 1;
+    const monthPosted = `${datePosted.getMonth() + 1}`.padStart(2, '0');
     shelljs.mkdir('-p', `${process.cwd()}/${outputDir || 'archive'}/${yearPosted}`);
     shelljs.mkdir('-p', `${process.cwd()}/${outputDir || 'archive'}/${yearPosted}/${monthPosted}`);
     fs.writeFileSync(`${process.cwd()}/${outputDir || 'archive'}/${yearPosted}/${monthPosted}/${dashify(post.post_title)}.md`, post.template);
